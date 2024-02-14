526319b019bfc1a4f5cda8dcdc332fd92f70e590
Sergio <se.diaz60@gmail.com>
2024-02-14 09:53:54 -0500

    [Update]: login con usuarios y role en dashboard

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@angular/core';
 import { SessionStorageService } from './session-storage.service';
 import { environment } from 'src/environments/environment';
+import infoUsers from 'src/assets/resources/users.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
   isAuthenticated: boolean;
+  roleUser: string;
 
   constructor(
     private sessionStorage: SessionStorageService
   ) {
     this.isAuthenticated = false;
+    this.roleUser = '';
   }
 
-  async login(username: string, password: string) {
+  async login(username: string, password: string, token: string) {
     try {
-      this.isAuthenticated = true;
-      this.sessionStorage.insert(environment.keySession, { username, password });
+      let usersList: any = infoUsers.users.find((u: any) => u.user === username && u.password === password && u.token === token );
 
-      return true;
+      if (usersList) {
+        this.isAuthenticated = true;
+        this.roleUser = usersList.role
+        this.sessionStorage.insert(environment.keySession, usersList);
+        return {'status': true, 'message': 'Login successful.'};
+      }
+
+      return {'status': false, 'message': 'user not found'};
 
     } catch (e: any) {
       console.error('login', e.error);
-      return false;
+      return {'status': false, 'message': { ...e.error }};
     }
   }
 
@@ -31,14 +40,18 @@ export class AuthServiceService {
     try {
       this.sessionStorage.remove(environment.keySession);
       this.isAuthenticated = false;
-      return true;
+      return {'status': true, 'message': 'Logout successful.'};
     } catch (e: any) {
       console.error('logout', e.error);
-      return false;
+      return {'status': false, 'message': { ...e.error }};
     }
   }
 
   isAuthenticatedUser(): boolean {
     return this.isAuthenticated;
+  }
+
+  isRoleUser(): string {
+    return this.roleUser;
   }
 }
